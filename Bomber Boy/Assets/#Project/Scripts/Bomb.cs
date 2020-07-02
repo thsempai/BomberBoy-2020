fd5a13997236caae3588ac3b5d877d2c8933b213
Thomas Stassin <thomas@panopticgame.com>
2020-07-02 16:24:37 +0200

    first version of gameplay

---
+++ Bomber Boy/Assets/#Project/Scripts/Bomb.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public float range = 5f;
+    public float timer = 3f;
+    public float force = 500f;
+
+    public GameObject[] walls;
+    public GameObject player;
+    public float firstImpulse=100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        walls = GameObject.FindGameObjectsWithTag("Wall");
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * firstImpulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        
+        if(timer <= 0) {
+            if (player) {
+                if (Vector3.Distance(player.transform.position, transform.position) <= range) {
+                    Destroy(player);
+                }
+            }
+            foreach (GameObject wall in walls) {
+                Rigidbody rb = wall.GetComponent<Rigidbody>();
+                rb.AddExplosionForce(force, transform.position, range);
+            }
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDrawGizmos() {
+        Color color = new Color(1f, 0.5f, 0.25f, 0.75f);
+        Gizmos.color = color;
+
+        Gizmos.DrawSphere(transform.position, range);
+    }
+}
---
+++ Bomber Boy/Assets/#Project/Scripts/BomberAI.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class BomberAI : MonoBehaviour {
+    private NavMeshAgent agent;
+    public Transform[] targets;
+    public GameObject bomb;
+
+    public GameObject player;
+    public float viewRange = 40f;
+    public float time = 1f;
+    private float actualTimer = 0f;
+
+    private bool playerFound;
+
+    // Start is called before the first frame update
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = GiveDestination();
+        if (!player)
+            player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        RaycastHit hit;
+
+        if(actualTimer > 0) {
+            actualTimer -= Time.deltaTime;
+        }
+
+        if (player) {
+            Vector3 direction = (player.transform.position - transform.position).normalized;
+
+            playerFound = false;
+
+            if (Physics.Raycast(transform.position, direction, out hit, viewRange)) {
+                if (hit.transform.gameObject == player) {
+                    playerFound = true;
+                    agent.destination = player.transform.position;
+                    if(hit.distance <= 5f) {
+                        if(actualTimer <= 0) {
+                            ThrowBomb();
+                            actualTimer = time;
+                        }
+                    }
+                }
+            }
+        }
+        if (IsArrived()) {
+            agent.destination = GiveDestination();
+        }
+    }
+
+    private Vector3 GiveDestination() {
+        int index = Random.Range(0, targets.Length);
+        return targets[index].position;
+    }
+
+    private bool IsArrived() {
+        return agent.remainingDistance <= agent.stoppingDistance;
+    }
+
+    private void OnDrawGizmos() {
+        Color color = Color.green;
+        if (playerFound) {
+            color = Color.red;
+        }
+        color.a = 0.5f;
+        Gizmos.color = color;
+        Gizmos.DrawSphere(transform.position, viewRange);
+    }
+
+    private void ThrowBomb() {
+        Vector3 position = transform.position + transform.forward * 0.5f;
+        GameObject bombInstance = Instantiate(bomb, position, transform.rotation);
+        bombInstance.GetComponent<Bomb>().player = player;
+    }
+}
